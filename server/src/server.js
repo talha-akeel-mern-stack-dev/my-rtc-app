6d860c6a9fe22b69d508f3f00339fb73ae03e6a8
Muhammad Talha Akeel <intellexadigital@gmail.com>
2026-09-08 12:23:37 +0500

    Add cron job to send test check requests every 14 minutes

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import fs from "fs";
 import path from "path";
+import job from "./lib/cron.js";
 
 const app = express();
 
@@ -34,4 +35,7 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on PORT:${PORT}`);
+
+    if(process.env.NODE_ENV === "production") job.start();
+
 });
