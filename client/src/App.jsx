6c850363bb85cb1ea1bcb25034f2605135187f03
Muhammad Talha Akeel <intellexadigital@gmail.com>
2026-09-12 18:21:03 +0500

    frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
+import { WallpaperProvider } from "./context/WallpaperContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from "@clerk/react";
 
 function App() {
+
+  const { isSignedIn, isLoaded } = useAuth();
+  if(!isLoaded) return <p>Loading ...</p>;
   
   return (
-    <div>
-      <h1>My RTC App</h1>
-
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode='modal' />
-          <SignUpButton mode='modal' />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+    <WallpaperProvider>
+      <Routes>
+        <Route path='/' element={ isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace /> } />
+        <Route path='/auth' element={ !isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace /> } />
+      </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
